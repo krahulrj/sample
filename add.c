674a9069815b01dc1417528bdc1a22c521b9dbcb
krahulrj <krahulrj24@gmail.com>
2024-06-24 16:06:05 +0530

    Update add.c

--- add.c
+++ add.c
@@ -1,1 +1,10 @@
-
+#include<stdio.h>
+void main()
+{
+   int a,b ;
+  printf("Enter First Number :");
+  scanf("%d",&a);
+  printf("Enter Second Number :");
+  scanf("%d",&b);
+  printf("Sum of First Number: %d and Second Number: %d is :%d",a,b,a+b);
+}
